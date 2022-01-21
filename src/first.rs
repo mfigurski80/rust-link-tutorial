e3a1788eabd6090ced318f06c6495affe6205c0e
Mikolaj Figurski <meek.f80@gmail.com>
2022-01-20 19:54:52 -0500

    Init list structures

--- src/first.rs
+++ src/first.rs
@@ -1,7 +1,19 @@
-#[derive(Debug)]
-pub enum List {
-    Empty,
-    Elem(i32, Box<List>),
+pub struct List {
+    head: Link,
 }
 
-fn main() {}
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: List,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+}
